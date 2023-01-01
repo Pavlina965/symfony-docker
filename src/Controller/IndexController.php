a4ae5a485c3ab27a8eef248c72565ca57e113428
Pavlina <pavlina965@gmail.com>
2023-01-01 19:21:46 +0100

    creating database of articles

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
+use MongoDB\Driver\Manager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route(name: 'app_index')]
+    public function createArticle(ManagerRegistry $doctrine): Response
+    {
+        $articleManager = $doctrine->getManager();
+        $article = $doctrine->getRepository(Article::class)->findAll();
+        return $this->render("index/index.html.twig",['articles'=>$article]);
+    }
+
+}
