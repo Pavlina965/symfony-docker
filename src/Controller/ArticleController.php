64cff34396adb9562e33f4c52acffd2606cb93c9
Pavlina <pavlina965@gmail.com>
2023-02-06 21:30:32 +0100

    show article by id, delete, create

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use DateTime;
 use Doctrine\Persistence\ManagerRegistry;
+use http\Exception\BadMethodCallException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,26 +13,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
 
-    #[Route('/article', name: 'app_article')]
+    #[Route('/article', name: 'article_create')]
     public function createArticle(ManagerRegistry $doctrine): Response
     {
-       $entityManager = $doctrine->getManager();
+        $entityManager = $doctrine->getManager();
 
-       $article = new Article();
-       $article->setName('New Article');
-       $article->setContent('This is new article.');
-       $article->setDate(new DateTime());
-       $entityManager->persist($article);
-       $entityManager ->flush();
+        $article = new Article();
+        $article->setName('New fcking Article');
+        $article->setContent('This is really new article.');
+        $article->setDate(new DateTime());
+        $entityManager->persist($article);
+        $entityManager->flush();
 
-       return new Response('new article with ID ' . $article->getId());
+        return $this->redirectToRoute('app_index');
+        //return new Response('new article with ID ' . $article->getId());
 
     }
 
     #[Route('article/{id}', name: 'article_show', methods: 'GET')]
     public function articleShow(Article $article): Response
     {
+
         return $this->render('article/index.html.twig', ['article' => $article]);
+    }
+
+    #[Route('article/delete/{id}', name: 'article_delete')]
+    public function articleDelete(ManagerRegistry $doctrine, Article $article, int $id): Response
+    {
+        $entityManager=$doctrine->getManager();
+        $article=$doctrine->getRepository(Article::class)->find($id);
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'notice','Article with id ' .$id.' has been deleted.'
+        );
+
+        return $this->redirectToRoute('app_index');
     }
 
 }
